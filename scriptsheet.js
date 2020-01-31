9b12cc570ce9d51a00ebeeebedf41f02ee376992
JawwadK <32138927+JawwadK@users.noreply.github.com>
2020-01-31 17:13:26 -0500

    Added stylesheet and scripts

---
+++ scriptsheet.js
@@ -0,0 +1,9 @@
+var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 60,
+    startDelay: 500,
+    backSpeed: 40,
+    backDelay: 1250,
+    loop: true,
+    loopCount: Infinity,
+  });
